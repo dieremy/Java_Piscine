cb3672ce3cd3d4298a0503041491d47feb477947
Roberto Remy Garcia <robegarc@e3r12p3.42roma.it>
2023-07-03 21:08:42 +0200

    done ex01

--- java_module_00/ex01/Program.java
+++ java_module_00/ex01/Program.java
@@ -1,12 +1,8 @@
 import java.util.Scanner;
 
-// PRIME NUMBERS TILL 100
-// 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
-// 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97.
-
 public class Program
 {
-	static int isPrime( int nb )
+	static int isPrimeIter( int nb )
 	{
 		int	i;
 
@@ -16,35 +12,43 @@ public class Program
 		while ( i <= ( nb / 2 ) )
 		{
 			if ( (nb % i) == 0 )
-				return (0);
-			else
-				i++;
+				break ;
+			i++;
 		}
-		return ( i );
+		return (i);
+	}
+
+	static boolean isPrimeBool( int nb )
+	{
+		int	i;
+
+		i = 2;
+		if ( nb < 2 )
+			return ( false );
+		while ( i <= ( nb / 2 ) )
+		{
+			if ( (nb % i) == 0 )
+				return ( false );
+			i++;
+		}
+		return ( true );
 	}
 
     public static void main(String[] args)
     {
 		Scanner myObj = new Scanner( System.in );  // Create a Scanner object
-		// System.out.println("ENTER NUMBER PLZ ");
-
 		int number = myObj.nextInt();  // Read user input
+
 		if ( number < 2 )
 		{
 			System.out.println( "IllegalArgument" );
 			System.exit( -1 );
 		}
-		int	check = isPrime( number );
-		if ( check != 0 )
-		{
-			// System.out.println( number + " is a prime number" );
-			System.out.println( "true " + check );
-		}
+		int	checkIter = isPrimeIter( number ) - 1;
+		boolean	checkBool = isPrimeBool( number );
+		if ( checkBool )
+			System.out.println( checkBool + " " + checkIter );
 		else
-		{
-			// System.out.println( number + " is NOT a prime number" );
-			System.out.println( "false " + check );
-
-		}
+			System.out.println( checkBool + " " + checkIter );
     }
 }
