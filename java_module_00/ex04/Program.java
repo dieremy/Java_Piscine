bb0d1aac0aca0c6fabdba26ae394f2019816cced
dieremy <100817004+dieremy@users.noreply.github.com>
2023-07-05 23:45:58 +0200

    wip ...

--- java_module_00/ex04/Program.java
+++ java_module_00/ex04/Program.java
@@ -6,7 +6,7 @@ public class Program {
     private static int[] countCharacterOccurrences(String text) {
         int[] occurrences = new int[65536]; // Assuming Unicode BMP
         char[] chars = text.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
+        for (int i = 0; i < text.length(); i++) {
             char c = chars[i];
             occurrences[c]++;
         }
@@ -14,24 +14,9 @@ public class Program {
     }
 
     // Sort characters by occurrence count and lexicographic order
-    private static char[] sortCharactersByOccurrences(int[] occurrences) {
-        int index = 0;
-        for (int count : occurrences)
-        {
-            if (count > 0)
-                index++;
-        }
-
-        char[] characters = new char[index]; // Assuming Unicode BMP
-        int x = 0;
-        for ( char i = 0; i < occurrences.length; i++ ) {
-            if ( occurrences[i] > 0 )
-                characters[x++] = i;
-        }
-        // System.out.print( "dioporco2" );
-
-        for (int i = 0; i < index - 1; i++) {
-            for (int j = 0; j < index - i - 1; j++) {
+    private static void sortCharactersByOccurrences(char[] characters, int[] occurrences) {
+        for (int i = 0; i < characters.length - 1; i++) {
+            for (int j = 0; j < characters.length - i - 1; j++) {
                 if (occurrences[characters[j]] < occurrences[characters[j + 1]] ||
                         (occurrences[characters[j]] == occurrences[characters[j + 1]] &&
                                 characters[j] > characters[j + 1])) {
@@ -41,60 +26,59 @@ public class Program {
                     characters[j + 1] = temp;
                 }
             }
-
         }
-        return characters;
     }
 
-    // Display histogram
+    // Display histogram with the top 10 most frequent characters
     private static void displayHistogram(char[] characters, int[] occurrences) {
-        int maxOccurrences = findMaxOccurrences(occurrences);
+        // Display only the top 10 most frequent characters
+        int maxOccurrences = occurrences[characters[0]];
         int scaleFactor = (maxOccurrences + 9) / 10; // Scale factor for proportional display
 
         for (int i = 10; i > 0; i--) {
-            for (int j = 0; j < characters.length; j++) {
-                int count = occurrences[characters[j]];
-                int scaledCount = (count + scaleFactor - 1) / scaleFactor;
-                if (scaledCount >= i) {
-                    System.out.print("# ");
-                } else {
-                    System.out.print("  ");
+            for (int j = 0; j < 10; j++) {
+                if (j < characters.length) {
+                    int count = occurrences[characters[j]];
+                    int scaledCount = (count + scaleFactor - 1) / scaleFactor;
+                    if (scaledCount >= i) {
+                        System.out.print("# ");
+                    } else {
+                        System.out.print("  ");
+                    }
                 }
             }
             System.out.println();
         }
 
         // Print character labels
-        for (char c : characters) {
-            System.out.print(c + " ");
-        }
-    }
-
-    // Find the maximum number of occurrences
-    private static int findMaxOccurrences(int[] occurrences) {
-        int max = 0;
-        for (int count : occurrences) {
-            if (count > max) {
-                max = count;
+        for (int j = 0; j < 10; j++) {
+            if (j < characters.length) {
+                System.out.print(characters[j] + " ");
             }
         }
-        return max;
     }
 
     public static void main(String[] args) {
         // Read input text
         Scanner scanner = new Scanner(System.in);
-        // System.out.print("Enter the input text: ");
         String text = scanner.nextLine();
-        // scanner.close();
-
+        scanner.close();
 
         // Count character occurrences
         int[] occurrences = countCharacterOccurrences(text);
 
-        // Sort characters by occurrence count and lexicographic order
-        char[] characters = sortCharactersByOccurrences(occurrences);
-        System.out.print( "dioporco" );
+        // Create an array to hold all possible characters
+        char[] characters = new char[65536];
+        int charIndex = 0;
+        for (int i = 0; i < occurrences.length; i++) {
+            if (occurrences[i] > 0) {
+                characters[charIndex] = (char) i;
+                charIndex++;
+            }
+        }
+
+        // Sort characters by occurrence count
+        sortCharactersByOccurrences(characters, occurrences);
 
         // Display histogram
         displayHistogram(characters, occurrences);
