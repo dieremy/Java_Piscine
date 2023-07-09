9ee34379b360c3446eb56ed04cd45e0a1e06e832
Roberto Remy Garcia <robegarc@e3r6p3.42roma.it>
2023-07-09 17:30:37 +0200

    wip ex02

--- java_module_01/ex02/Program.java
+++ java_module_01/ex02/Program.java
@@ -1,3 +1,19 @@
+public class Program
+{
+	public static void main( String[] args )
+	{
+		User[] users = new User[5];
+		for ( int i = 0; i < 5; i++ )
+			users[i] = new User();
+
+		UsersArrayList userList = new UsersArrayList();
+		for ( int i = 0; i < 5; i++ )
+			userList.addUser( users[i] );
+	}
+}
+
+
+
 /// public interface UserList ( .java )
 /// methods declaration:
 /// void addUser( User new )
@@ -10,7 +26,7 @@
 /// getUserById return the user[i] when iter matches /// if not, throw new exception
 /// getUserByIndex returns User[i] where i is passed as argument /// if not, throw new exception
 /// getUserCount - getter
-/// printInof loop to print each User 
+/// printInof loop to print each User
 
 /// public class UserNotFoundException ( .java ) extends RuntimeException
 /// UserNotFoundException( String msg )
