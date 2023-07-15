6b42b221b22213cef5b7234cd7da4ce0927c5503
parrot <parrot@localhost.localdomain>
2023-07-15 22:19:30 +0200

    wip...

--- java_module_03/ex02/Program.java
+++ java_module_03/ex02/Program.java
@@ -1,7 +1,11 @@
+import java.util.Random;
+
 class Program
 {
     private static int arraySize;
     private static int threadsCount;
+    private static int chunks;
+	private static int sum;
 
     public static void main( String[] args )
 	{
@@ -10,22 +14,33 @@ class Program
 			if ( !checkArguments( args ) )
 				System.exit( -1 );
             System.out.println( "arraySize: " + arraySize + "\tthreadsCount: " + threadsCount );
-            // Egg egg = new Egg( count );
-            // Hen hen = new Hen( count );
-
-            // egg.start();
-            // hen.start();
-            // egg.join();
-            // hen.join();
-
-            // for ( int i = 0; i < count; i++ )
-            // 	System.out.println( "Human" );
+            
+			System.out.println();
+			int[] vector = initArray();
+			for ( int i = 0; i < vector.length; i++ )
+				System.out.println( "vector[i]: " + vector[i] );
+            System.out.println();
+			
+			System.out.println( "Sum: " + sum );
 		}
 		catch ( Exception e )
 		{
 			System.out.println( "Invalid input. Enter a valid number." );
 			System.out.println( e.getMessage() );
 		}
+	}
+
+	public static int[] initArray()
+	{
+		Random	r = new Random();
+		int[]	vector = new int[arraySize];
+
+		for ( int i = 0; i < vector.length; i++ )
+		{
+			vector[i] = r.nextInt(50);
+			sum += vector[i];
+		}
+		return ( vector );
 	}
 
 	public static boolean checkArguments( String[] args )
@@ -39,9 +54,9 @@ class Program
 		{
 			arraySize = Integer.parseInt( args[0].substring( 12 ) );
 			threadsCount = Integer.parseInt( args[1].substring( 15 ) );
-            if ( arraySize < 1 || threadsCount < 1 )
+            if ( arraySize > 2_000_000 || arraySize < 1 || threadsCount < 1 || threadsCount > arraySize )
             {
-                System.err.println( "Please test using numbers > 0" );
+                System.err.println( "Illegal argument for arraySize or threadsCount" );
                 return ( false );
             }
 		}
