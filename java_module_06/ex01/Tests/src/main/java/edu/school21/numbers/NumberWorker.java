b2ccb9b4a69be46df1a15e25cd23af8182c2f5b7
parrot <parrot@localhost.localdomain>
2023-07-27 16:52:08 +0200

    update

---
+++ java_module_06/ex01/Tests/src/main/java/edu/school21/numbers/NumberWorker.java
@@ -0,0 +1,41 @@
+package edu.school21.numbers;
+
+public class NumberWorker
+{
+    public boolean isPrime( int number )
+	{
+		int	i;
+
+		i = 2;
+		if ( number < 2 )
+            throw new IllegalNumberException();
+		while ( i <= ( number / 2 ) )
+		{
+			if ( ( number % i ) == 0 )
+				return ( false );
+			i++;
+		}
+		return ( true );
+	}
+
+	public int digitsSum( int number )
+    {
+        int sum;
+
+        sum = 0;
+        while ( number != 0 )
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        return ( sum );
+    }
+
+    class IllegalNumberException extends RuntimeException
+    {
+        public IllegalNumberException()
+        {
+            super( "Input number has to be > 1." );
+        }
+    }
+}
